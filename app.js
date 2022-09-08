39719524e856557f837bbc7c4b8ff519ac3b3ec3
Chamath Jayarathne <chamathsocial@gmail.com>
2022-09-07 20:02:53 -0400

    feat[nav buttons]: Add tooltips to nav buttons and theme button

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ function themeTransitions(){
 
     // toggle theme
     const themeBtn = document.querySelector('.theme-btn');
+    const tooltip = themeBtn.querySelector('.tooltip');
+
     themeBtn.addEventListener('click', () =>{
         let element = document.body;
         element.classList.toggle('light-mode');
-    })
+
+        if (tooltip.innerText === 'Light Mode'){
+            tooltip.innerText = 'Dark Mode';
+        } else {
+            tooltip.innerText = 'Light Mode';
+        }
+
+    });
 }
 
 themeTransitions();
-
-
-function imageTransition(){
-
-}
-
-imageTransition();
